f0ac7df0ceb21fb0c5f0f0dd0c89ac9e6cb256ac
sc4ramouche <vladislav.kovechenkov@gmail.com>
2021-02-21 01:02:11 +0300

    Sample content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React from 'react';
+import '@fontsource/ubuntu';
+
+import Button from '../components/Button';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div
+      style={{
+        color: `#333`,
+        fontSize: `56px`,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Hey there 👋</h1>
+      <p>I'm Vlad. Here I'll share my thoughts and ideas. Soon!</p>
+    </div>
+  );
 }
