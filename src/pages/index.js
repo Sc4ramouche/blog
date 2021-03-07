b9286e56d2df9e96c79e8cfdc5f72b08eeffa4bd
sc4ramouche <vladislav.kovechenkov@gmail.com>
2021-03-07 13:49:52 +0300

    Add Page wrapper component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import '@fontsource/ubuntu';
 
+import Page from '../components/Page';
 import Button from '../components/Button';
 
 export default function Home() {
   return (
-    <div
-      style={{
-        color: `#333`,
-        fontSize: `56px`,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <h1>Hey there 👋</h1>
-      <p>I'm Vlad. Here I'll share my thoughts and ideas. Soon!</p>
-    </div>
+    <Page>
+      <div
+        style={{
+          color: `#333`,
+          fontSize: `56px`,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Hey there 👋</h1>
+        <p>I'm Vlad. Here I'll share my thoughts and ideas. Soon!</p>
+      </div>
+    </Page>
   );
 }
