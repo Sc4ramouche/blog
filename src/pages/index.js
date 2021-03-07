03323d0d2965c296073370162cc016c746268933
sc4ramouche <vladislav.kovechenkov@gmail.com>
2021-03-07 18:46:07 +0300

    Add the list of posts to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,11 @@ import '@fontsource/ubuntu';
 import Page from '../components/Page';
 import SEO from '../components/SEO';
 import Button from '../components/Button';
+import { graphql, Link } from 'gatsby';
 
-export default function Home() {
+export default function Home({ data }) {
+  const posts = data.allMdx.nodes;
+
   return (
     <Page>
       <SEO title={'Home'} />
@@ -20,7 +23,25 @@ export default function Home() {
       >
         <h1>Hey there 👋</h1>
         <p>I'm Vlad. Here I'll share my thoughts and ideas. Soon!</p>
+        {posts.map(({ frontmatter: { title }, slug }) => (
+          <Link to={slug} key={slug}>
+            <h4>{title}</h4>
+          </Link>
+        ))}
       </div>
     </Page>
   );
 }
+
+export const postsQuery = graphql`
+  {
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+      nodes {
+        slug
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+`;
